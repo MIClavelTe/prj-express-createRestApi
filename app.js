9868820e6fc234e080b989a882a7db257a442f7b
MIClavelTe <miclavelte@gmail.com>
2018-09-22 12:36:22 -0700

    Edit static route

--- app.js
+++ app.js
@@ -3,12 +3,7 @@ var path = require('path')
 var express = require('express')
 var app = express()
 
-app.use(express.static(path.join(__dirname, './src/public')))
-
-app.get('/', function(req, res) {
-    // express.static('./src/public/index.html')
-    res.send('Hello')
-})
+app.get('/', express.static(path.join(__dirname, './src/public')))
 
 app.listen(3000, function() {
     console.log('Running on localhost:3000')
