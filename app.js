e8a99f737d897a8598d1e2a516ca9fb0b09bc217
MIClavelTe <miclavelte@gmail.com>
2018-09-21 23:06:10 -0700

    Add static route

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+var path = require('path')
+
 var express = require('express')
 var app = express()
 
+app.use(express.static(path.join(__dirname, './src/public')))
+
 app.get('/', function(req, res) {
+    // express.static('./src/public/index.html')
     res.send('Hello')
 })
 
