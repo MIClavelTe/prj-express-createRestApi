391085017ee71d852c3c6f4e6e8295d36e089dc0
MIClavelTe <miclavelte@gmail.com>
2018-09-23 12:05:06 -0700

    Install mongoose

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var path = require('path')
 
 var express = require('express')
 var app = express()
+var mongoose = require('mongoose')
 var princessRoutes = require('./routes/princessRoutes')
 var bookRoutes = require('./routes/bookRoutes')
+
+mongoose.connect('mongodb://localhost:27017/createRestApi');
+var db = mongoose.connection;
 
 app.use(express.static(path.join(__dirname, './src/public')))
 
